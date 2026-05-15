6b80e0ed157e831b21f7abbb54a291d605bb63f4
Andrew Red <andrew@reddikh.com>
2026-05-15 10:06:16 +0100

    - fixed parallel test execution

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    fileParallelism: false,
   },
   resolve: {
     alias: {
